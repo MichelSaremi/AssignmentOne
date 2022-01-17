2325edb7dc3e26026324b2cca2c57c82301ccdf3
Michel Saremi <michel.saremi@outlook.dk>
2022-01-17 08:37:51 +0100

    Fixed the image bug and commenting

--- Komputor.js
+++ Komputor.js
@@ -1,24 +1,26 @@
 /////////////////////////Bank part
 
+//refering to the html elements
 let loanSum = document.getElementById("loanT")
 let loanText = document.getElementById("loanText")
 loanText.style.display ='none'
 
-//let balance = document.getElementById("balance")
 let balanceSum = Number(document.getElementById("balance").innerText)
 
 let MaxLoan = balanceSum*2
 
 let buttonLoan = document.getElementById('loan')
 
+//To happen when loan button is clicked
 buttonLoan.onclick = () => {
 	
     MaxLoan = (Number(document.getElementById("balance").innerText))*2
 
+    //window with input request
     const loanRequest = Number(window.prompt("How much would you like to loan?\n You can loan maximum: "+MaxLoan));
     loanText.style.display='block'
 
-
+    //input processing
     if (Number(loanSum.innerText) != 0){
         alert("You cannot have more than one active loan at a time")
     }else{
@@ -43,7 +45,7 @@ buttonLoan.onclick = () => {
 let buttonWork = document.getElementById('WorkButton')
 let PaySum = document.getElementById("pay")
 
-
+//When work button is clicked
 buttonWork.onclick = () => {
     let PaySumNumber = Number(document.getElementById("pay").innerText)
     PaySumNumber = PaySumNumber + 100
@@ -52,6 +54,8 @@ buttonWork.onclick = () => {
 } 
 
 ///Bank button
+
+// relevant references to the html elements
 let buttonBank = document.getElementById('BankButton')
 
 let loanSum2 = document.getElementById("loanT")
@@ -59,7 +63,10 @@ let loanSum2 = document.getElementById("loanT")
 let balanceSum2 = document.getElementById("balance")
 let balanceSum2Number = Number(document.getElementById("balance").innerText)
 
+//When bank button is clicked
 buttonBank.onclick = () => {
+    
+    //what happens when there is an active loan
     let loanSum2Number = Number(document.getElementById("loanT").innerText)
     if (loanSum2Number!=0){
         let PaySumNumber = Number(document.getElementById("pay").innerText)
@@ -75,11 +82,12 @@ buttonBank.onclick = () => {
         PaySumNumber = 0
         PaySum.innerText = 0
         
-        
+        //what happens when loan is negative
         if (loanSum2Number<0){
             loanSum2Number = 0
             loanSum2.innerText = loanSum2Number
         }
+    //What happens when there is no loan   
     }else if (loanSum2Number==0){
         let PaySumNumber = Number(document.getElementById("pay").innerText)
         balanceSum2Number = balanceSum2Number + PaySumNumber
@@ -95,7 +103,10 @@ buttonBank.onclick = () => {
 let buttonRepay = document.getElementById('RepayButton')
 buttonRepay.style.display ='none'
 
+//When repay loan button is clicked
 buttonRepay.onclick = () => {
+    
+    //Make html references
     let loanSum3 = document.getElementById("loanT")
     let loanSum3Number = Number(document.getElementById("loanT").innerText)
 
@@ -104,12 +115,14 @@ buttonRepay.onclick = () => {
     let balance3 = document.getElementById("balance")
     let balance3Number = Number(document.getElementById("balance").innerText)
    
+    //code if loan is higher than pay
     if (loanSum3Number>PaySumNumber){
         Result = loanSum3Number - PaySumNumber
         loanSum.innerText = Result
         PaySumNumber = 0
         PaySum.innerText = PaySumNumber
-
+    
+    //code if loan is lower than pay
     }else if(PaySumNumber>loanSum3Number){
         Result = PaySumNumber - loanSum3Number
         balance3Number = balance3Number + Result
@@ -120,6 +133,7 @@ buttonRepay.onclick = () => {
         PaySumNumber = 0
         PaySum.innerText = PaySumNumber
 
+    //code if loan and pay are equal
     }else{
         PaySumNumber = 0
         PaySum.innerText = PaySumNumber
@@ -130,7 +144,9 @@ buttonRepay.onclick = () => {
 
 //////////////////////////////////////////Laptop part
 
-//////Laptop selection
+////////Laptop selection section
+
+//Arrays to store data
 const laptopList = document.getElementById("laptopList")
 const laptopTitle = [];
 const laptopSpecs = [];
@@ -138,6 +154,7 @@ const laptopDescrip = [];
 const laptopPrice = [];
 const laptopImage = [];
 
+//Fetching the date to populate above arrays
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
 
 .then(function(response){
@@ -152,7 +169,7 @@ fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
         laptopPrice.push(laptop.price)
         laptopImage.push(laptop.image)
     }
-    //////////populationg dropdown list
+    //populationg the html dropdown list
     for(let i=0; i<laptopTitle.length; i++){
         const newItem = document.createElement("option")
         newItem.value = laptopTitle[i]
@@ -164,16 +181,22 @@ fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
     console.log(error)
 })
 
+////Adding the laptop specs when choice from the dropdown list is made 
+
+//These to lines of code belong to the laptopinfo sections image display 
 const ImagePlace = document.getElementById("image")
 ImagePlace.style.display ='none'
+
 let ChoiceLargerScope = ""
 let PriceNumberLargerScope = ""
 
+//Function to run when choice is made
 function getValueAndDisplay(){
     const Choice = document.getElementById("laptopList").value
     ChoiceLargerScope = Choice
     const features = document.getElementById("info")
     
+    //Displaying the features text
     specList = []
     let index = laptopTitle.indexOf(Choice)
     let specText = laptopSpecs[index]
@@ -182,12 +205,15 @@ function getValueAndDisplay(){
 
     }
     features.innerText = specList
-    ///////////////laptop info
+    
+    ///////////////laptop info section
 
+    //references to html elements
     const Header = document.getElementById("LaptopHeader")
     const Description = document.getElementById("LaptopDescription")
     const Price = document.getElementById("LaptopPrice")
 
+    //Adding info based on choice
     Header.innerText = Choice
 
     DescriptionText = laptopDescrip[index]
@@ -197,26 +223,38 @@ function getValueAndDisplay(){
     Price.innerText = PriceText+" "
     PriceNumber = Number(PriceText)
     PriceNumberLargerScope = PriceNumber
-    //////////////////Laptop images
+
+    //Laptop images configuration
     ImagePlace.style.display ='block'
     ImagePlace.style.height = '150px'
     ImagePlace.style.width = '150px'
     
-    let image = laptopImage[index]
-
-    let imageURL = "https://noroff-komputer-store-api.herokuapp.com/"+image
-
-    ImagePlace.src = imageURL
+    //Display image of laptop based on choice
+    if (ChoiceLargerScope == "The Visor"){
+        let imageURL = "https://noroff-komputer-store-api.herokuapp.com/assets/images/5.png"
+        ImagePlace.src = imageURL
+    }else{
+        let image = laptopImage[index]
+        let imageURL = "https://noroff-komputer-store-api.herokuapp.com/"+image
+        ImagePlace.src = imageURL
+    }
 }
 
-//////////////////The buy button
+///The buy button
+
+//referencing to html elements
 let buttonBuy = document.getElementById("BuyButton")
 let balanceSum3 = document.getElementById("balance")
 
+//code when buy button is clicked
 buttonBuy.onclick = () => { 
         let balanceSum3Number = Number(document.getElementById("balance").innerText)
+        
+        //Code if funds are insufficient 
         if (PriceNumberLargerScope>balanceSum3Number){
             alert("You do not have sufficient funds to buy this Laptop")
+        
+            //Code if funds are sufficient 
         }else if (balanceSum3Number>=PriceNumberLargerScope){
             alert("You are now the proud owner of the "+ChoiceLargerScope)
             balanceSum3Number = balanceSum3Number - PriceNumberLargerScope
